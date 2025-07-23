5e3a43ead6b5864cfa316f1f7e1c830e7870acd7
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-07-23 16:43:00 +0530

    props

--- React-Blog/Props/Props.jsx
+++ React-Blog/Props/Props.jsx
@@ -1,13 +1,29 @@
-import "../Multiple_Condition/Multiple.css"
+import "../styling/style.css"
 export function Props({username,gmail,coursename }){
     return(
-        <div className="multiple">
+        <div className="styling">
             <h1>Props: </h1>
             <h2>User Component: <br/></h2>
-                <p>{username}</p>
-                <p>{gmail}</p>
-                <p>{coursename}</p>
+                <p>Username: {username}</p>
+                <p>Gmail: {gmail}</p>
+                <p>Course Name:{coursename}</p>
 
         </div>
     )
 }
+
+/**
+ * yahan pe Props({yahan value destructure ho raha hai}) otherwise we can simply do like:
+ * Props{data}
+ * 
+ * {data.name}
+ * {data.gmail}
+ * 
+ * ham name kuch bhi likh sakhte hein data ke jagah mein likh sakhta hoon props. 
+ * aur aggar ham destructure karange toh aram se access kar sakhte hai without using . notation like : 
+ * {username}
+ * 
+ * name should be same here apne jo props mein bheja hai name wohi rahega. 
+ * aur apne galat name dala jaise ki age jo exist hi nahi karta toh hame milega undefined.
+ * 
+ */
